0a2fc20f02270c080be2c3f508ee27ba284e7485
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2022-08-12 19:35:34 +0530

    Credit lock bug fixed

--- creditlock.py
+++ creditlock.py
@@ -1,4 +1,5 @@
 import json 
+#import urllib.parse
 
 def getlockdetails(session,party_data) :  
   url_rec = '/rsunify/app/billing/partyinfo.do?partyId=_parId_&partyCode=_partyCode_&parCodeRef=_parCodeRef_&parHllCode=_parHllCode_&plgFlag=true&salChnlCode=&isMigration=true&blhSourceOfOrder=0'
@@ -16,16 +17,21 @@ def getlockdetails(session,party_data) :
 
 def releaselock(session,party_data) :  
   # party_data = { "parCode": "P-P18078" , "parCodeHll": "HUL-413724D-P3364","parCodeRef": "D-P18078" , "parId": 5582 }
-  url_rec = '/rsunify/app/billing/partyinfo.do?partyId=_parId_&partyCode=_parCode_&parCodeRef=_parCodeRef_&parHllCode=_parHllCode_&plgFlag=true&salChnlCode=&isMigration=true&blhSourceOfOrder=0'
+  url_rec = '/rsunify/app/billing/partyinfo.do?partyId=_parId_&partyCode=_partyCode_&parCodeRef=_parCodeRef_&parHllCode=_parHllCode_&plgFlag=true&salChnlCode=&isMigration=true&blhSourceOfOrder=0'
+  print(party_data)
   for key,value in party_data.items() : 
       url_rec = url_rec.replace('_'+key+'_',str(value))
   party_data = session.get(url_rec).json()
+  print(url_rec)
+  #input()
   print(party_data)
   response = {"parCodeRef":party_data["partyMasterCode"] ,"parCodeHll":party_data["partyHULCode"] ,"showPLG":party_data["showPLG"], "creditLimit":party_data["creditLimit"],
                  "creditDays":party_data["creditDays"],"newlimit":int(party_data["creditBillsUtilised"])+1  } 
+  print(response)
   url_send = '/rsunify/app/billing/updatepartyinfo.do?partyCodeRef=_parCodeRef_&creditBills=_newlimit_&creditLimit=_creditLimit_&creditDays=0&panNumber=&servicingPlgValue=_showPLG_&plgPartyCredit=true&parHllCode=_parCodeHll_'
   for key,value in response.items() : 
       url_send = url_send.replace('_'+key+'_',str(value))
+  
   url_send = session.get( url_send.replace('+','%2B') )
 
   
